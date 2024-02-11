7df648caf39d2878af8010de2c552f201d568510
Pratik Chavan <97500001+pratikychavan@users.noreply.github.com>
2024-02-11 14:46:45 +0530

    Delete credentials.sh

--- credentials.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-access_key=
-secret_key=
-region=
-aws configure set aws_access_key_id $access_key \
-&& aws configure set aws_secret_access_key $secret_key \
-&& aws configure set default.region $region \
-&& aws configure set default.output json
